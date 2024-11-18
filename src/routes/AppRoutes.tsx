75b3fc32e020d342f119099326c36b2cdbc433fd
Nistha <nistha984475@gmail.com>
2024-11-18 12:54:58 +0545

    fix authentication in publicRoutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,14 +3,17 @@ import ProtectedRoute from "./ProtectedRoutes";
 import { allRoutes } from "./Routes";
 import Layout from "../layout/Layout";
 import Notfound from "../pages/notFound/NotFound";
+import PublicRoutes from "./PublicRoutes";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        {allRoutes.publicRoutes.map((route) => (
-          <Route path={route.path} element={route.element} key={route.id} />
-        ))}
+        <Route path="/" element={<PublicRoutes />}>
+          {allRoutes.publicRoutes.map((route) => (
+            <Route path={route.path} element={route.element} key={route.id} />
+          ))}
+        </Route>
         <Route path="/" element={<ProtectedRoute />}>
           {allRoutes.protectedRoutes.map((route) => (
             <Route path={route.path} element={route.element} key={route.id} />
